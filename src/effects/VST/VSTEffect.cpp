d49eb7e43328f2a31d3832e9e82af9ca55b46cb1
pietro68 <pietro.marcello.68@tiscali.it>
2022-08-23 21:55:16 +0200

    1. Validator can fetch settings from, store to, Instance (and stores on construction)

--- src/effects/VST/VSTEffect.cpp
+++ src/effects/VST/VSTEffect.cpp
@@ -3563,7 +3563,11 @@ VSTEffectValidator::VSTEffectValidator
 )
    : DefaultEffectUIValidator(effect, access, pParent),
      mInstance(instance)
-{}
+{
+   // Make the settings of the instance up to date before using it to
+   // build a UI
+   StoreSettingsToInstance(mAccess.Get());
+}
 
 
 VSTEffectInstance& VSTEffectValidator::GetInstance()
@@ -3643,5 +3647,20 @@ VSTEffectInstance::~VSTEffectInstance()
    Unload();
 }
 
+
+bool VSTEffectValidator::FetchSettingsFromInstance(EffectSettings& settings)
+{
+   return mInstance.FetchSettings(
+      // Change this when GetSettings becomes a static function
+      static_cast<const VSTEffect&>(mEffect).GetSettings(settings));
+}
+
+
+bool VSTEffectValidator::StoreSettingsToInstance(const EffectSettings& settings)
+{
+   return mInstance.StoreSettings(
+      // Change this when GetSettings becomes a static function
+      static_cast<const VSTEffect&>(mEffect).GetSettings(settings));
+}
 
 #endif // USE_VST
