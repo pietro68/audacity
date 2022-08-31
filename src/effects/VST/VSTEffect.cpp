417f531f68659652df017b81e85b2d07658f1414
pietro68 <pietro.marcello.68@tiscali.it>
2022-08-31 17:47:18 +0200

    removed now useless mValidator member

--- src/effects/VST/VSTEffect.cpp
+++ src/effects/VST/VSTEffect.cpp
@@ -1156,7 +1156,6 @@ int VSTEffect::ShowClientInterface(
    //   mProcessLevel = 1;      // in GUI thread
 
    VSTEffectValidator* vstValidator = static_cast<VSTEffectValidator*>(validator);
-   mValidator = vstValidator;
 
    if (! vstValidator->GetInstance().IsReady() )
    {
@@ -1341,7 +1340,6 @@ bool VSTEffect::CloseUI()
 {
    mParentFE = NULL;
    mDialogFE = NULL;
-   mValidator = nullptr;
 
    return true;
 }
