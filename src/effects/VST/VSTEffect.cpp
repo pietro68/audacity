7fcc8c3dbd988c0f415f42e1150272833d742a76
pietro68 <pietro.marcello.68@tiscali.it>
2022-09-05 12:28:31 +0200

    corrected application of Unload()

--- src/effects/VST/VSTEffect.cpp
+++ src/effects/VST/VSTEffect.cpp
@@ -796,7 +796,6 @@ VSTEffect::VSTEffect(const PluginPath & path)
 
 VSTEffect::~VSTEffect()
 {
-   Unload();
 }
 
 // ============================================================================
@@ -1782,6 +1781,13 @@ void VSTEffectWrapper::ResetModuleAndHandle()
       mModule.reset();
       mAEffect = NULL;
    }
+}
+
+
+VSTEffectWrapper::~VSTEffectWrapper()
+{
+   Unload();
+   ResetModuleAndHandle();
 }
 
 
@@ -3620,7 +3626,6 @@ VSTEffectInstance::VSTEffectInstance
 VSTEffectInstance::~VSTEffectInstance()
 {
    PowerOff();
-   Unload();
 }
 
 
